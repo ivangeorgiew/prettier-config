f265c61642a28a6bd79d74c37f7bf7c28c16da6e
Ivan Georgiev <ivan_georgiew@yahoo.com>
2021-08-30 16:26:46 +0300

    switched to double quotes

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-module.exports = {
-    printWidth: 85,
-    tabWidth: 4,
-    useTabs: false,
-    semi: false,
-    singleQuote: true,
-    quoteProps: 'consistent',
-    jsxSingleQuote: true,
-    trailingComma: 'es5',
-    bracketSpacing: true,
-    jsxBracketSameLine: false,
-    arrowParens: 'avoid',
-    htmlWhitespaceSensitivity: 'css',
-    proseWrap: 'always',
-    endOfLine: 'lf',
-    embeddedLanguageFormatting: 'auto'
-}
+"use strict"
+
+module.exports = require("./.prettierrc.json")
